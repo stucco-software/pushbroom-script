417c455d15af13ad230701014229aaaa00ca4de5
nikolaswise <nikolaswise@gmail.com>
2023-01-10 21:40:34 -0800

    sveltekit all the things

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pushbroom Collector Script
+
+This is an analytics collector script based on the work from [Normally](https://github.com/Normally/cabin-script).
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<h1>Pushbroom Script</h1>
+<ul>
+  <li>
+    <a href="/index.js">index.js</a>
+  </li>
+  <li>
+    <a href="/index.min.js">index.min.js</a>
+  </li>
+</ul>
